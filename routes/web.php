2752690f4b493a73dd41c56e645ba5595745e13e
Mahrab hossen <mahrabbappy33@gmail.com>
2025-10-26 12:18:35 +0600

    created secondpagecontroller

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Admin\DashboardController;
-
+use App\Http\Controllers\secondpageController;
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/second-page', [secondpageController::class, 'contact'])->name('second.page');
 
 // Admin panel routes (only for authenticated users)
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
